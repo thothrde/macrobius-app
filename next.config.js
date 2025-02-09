b507f68a0dab586050b8027323d732ae94325ffc
thothrde <thomasriepe@gmail.com>
2025-02-09 16:33:50 +0100

    Update next.config.js with correct export settings

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: '/macrobius-app',
-  assetPrefix: '/macrobius-app/',
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/macrobius-app' : '',
   images: {
     unoptimized: true
   },
-  output: 'export',
+  reactStrictMode: true,
   trailingSlash: true
 }
 
