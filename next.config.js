4ea8b0acf2683fe7cc5ac51dd119a3592f573b85
thothrde <thomasriepe@gmail.com>
2025-02-09 16:31:40 +0100

    Add next.js config

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/macrobius-app',
+  assetPrefix: '/macrobius-app/',
+  images: {
+    unoptimized: true
+  },
+  output: 'export',
+  trailingSlash: true
+}
+
+module.exports = nextConfig
