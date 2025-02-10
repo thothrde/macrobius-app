342c80ee2a5b2b7f8fc9de2ab2a3bc651ce45319
thothrde <thomasriepe@gmail.com>
2025-02-10 08:38:37 +0100

    Simplify Next.js config with correct asset paths

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
   images: { unoptimized: true },
   trailingSlash: true,
-  experimental: {
-    appDir: false
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      path: false
-    };
-    return config;
-  }
+  assetPrefix: '.',
+  basePath: '',
 }
 
 module.exports = nextConfig
