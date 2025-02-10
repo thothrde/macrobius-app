948d47a48c1ad2f809d5c9bfbfea6206bd0646a4
thothrde <thomasriepe@gmail.com>
2025-02-10 08:44:19 +0100

    Update Next.js config with correct base path

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/macrobius-app',
+  assetPrefix: '/macrobius-app/',
   images: { unoptimized: true },
-  basePath: '',
-  assetPrefix: '',
   trailingSlash: true,
 }
 
